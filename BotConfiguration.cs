5d0483c58563514dac20c157c01e8ff114d4e908
Zerpico <zerpico@yandex.ru>
2019-07-03 22:19:44 +0500

    new synth

--- BotConfiguration.cs
+++ BotConfiguration.cs
@@ -7,7 +7,7 @@ namespace calendar_flood_bot
 {
     public class BotConfiguration
     {
-        public static string Token { get; set; } = "820488519:AAFjz2tf2KmSw5lX0CTWe0drWLWF4AB11sc";
+        public static string Token { get; set; } = "820488519:AAHypCuFfEed9zuP9xdSg2NjUABMjN3CccE";
         public static string Url { get; set; } = @"https://calendar-flood-bot.herokuapp.com/{0}";
     }
 }
